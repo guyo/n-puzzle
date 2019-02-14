1f5c97cd8aed6b56c34b38cbef86da4a5b85eaea
guyo <offer.guy@gmail.com>
2019-02-14 14:17:49 +0200

    add selenium and run it through jest

--- e2e/e2e.script.js
+++ e2e/e2e.script.js
@@ -1,5 +1,15 @@
-describe('empty test', () => {
-    it('should do nothing', () => {
-        expect(true);
+const { Builder } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+
+describe('run selenium', () => {
+    it('should be able to open a browser', async () => {
+        const driver = await new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(new chrome.Options().addArguments('incognito','headless','disable-gpu'))
+            .build();
+
+        await driver.manage().window().maximize();
+        await driver.quit();
     });
 });

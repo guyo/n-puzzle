5b0936287e7f8752439fa8f56ca5f99ecc6160b4
guyo <offer.guy@gmail.com>
2019-02-27 15:34:10 +0200

    fix script to handle snapshots

--- e2e/e2e.script.js
+++ e2e/e2e.script.js
@@ -1,39 +1,65 @@
-const { Builder } = require('selenium-webdriver');
+const { Builder, logging } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const GamePage = require('./GamePage.js');
-//const fs=require('fs');
+const fs=require('fs');
 
 require('chromedriver');
 
+const __URL__ = 'http://localhost:5000/t';
+const __MAXIMAL_ALLOWED_LOG_LEVEL = logging.Level.INFO;
+
 let driver;
 let gamePage;
-//let snapshot=1;
+let snapshot = 0;
 
+async function startGame(tiles) {
+    await gamePage.setBoard(tiles);
+    const ngm = await gamePage.getNewGameModal();
+    await ngm.setSize(Math.round(Math.sqrt(tiles.length)));
+}
+
+async function validateLogs() {
+    let entries = await driver.manage().logs().get(logging.Type.BROWSER);
+    entries.forEach(entry => console.log('[%s] %s', entry.level.name, entry.message));
+    entries = entries.filter(entry => entry.level.value > __MAXIMAL_ALLOWED_LOG_LEVEL.value);
+
+    expect(entries.length).toEqual(0);
+}
+
+async function onFail() {
+    const entries = await driver.manage().logs().get(logging.Type.BROWSER);
+    entries.forEach(entry => console.log('[%s] %s', entry.level.name, entry.message));
+
+    const image = await driver.takeScreenshot();
+    await fs.writeFile(`./sc${snapshot++}.png`, image, 'base64', (e) => { if (e) console.log(e); });
+}
+
+const pref = new logging.Preferences();
+pref.setLevel(logging.Type.BROWSER, logging.Level.ALL);
 
 beforeEach(async () => {
     driver = await new Builder()
         .forBrowser('chrome')
-        .setChromeOptions(new chrome.Options().addArguments('incognito', 'headless', 'disable-gpu'))
+        .setChromeOptions(new chrome.Options()
+            .addArguments('incognito', 'headless', 'disable-gpu')
+            .setLoggingPrefs(pref))
         .build();
 
     gamePage = new GamePage(driver);
 
     await driver.manage().window().maximize();
-    await driver.navigate().to('http://localhost:5000');
+    await driver.navigate().to(__URL__);
 });
 
 afterEach(async () => {
-    // console.log(await driver.manage().logs().get('browser'));
-    // const image=await driver.takeScreenshot();
-    // await fs.writeFile(`./sc${snapshot++}.png`, image,'base64',(e)=>{if (e) console.log(e)});
+    await onFail();
     await driver.quit();
 });
 
 
+
 test('test base mechanics', async () => {
-    await gamePage.setBoard([1, 2, 3, 4, null, 6, 7, 5, 8]);
-    const ngm = await gamePage.getNewGameModal();
-    await ngm.setSize(3);
+    await startGame([1, 2, 3, 4, null, 6, 7, 5, 8]);
 
     await gamePage.clickTile('2');
     await expect(gamePage.getTiles()).resolves.toEqual([['1', undefined, '3'], ['4', '2', '6'], ['7', '5', '8']]);
@@ -54,13 +80,12 @@ test('test base mechanics', async () => {
     await gamePage.clickTile('6');
     await expect(gamePage.getTiles()).resolves.toEqual([['1', '2', '3'], ['4', '6'], ['7', '5', '8']]);
 
+    await validateLogs();
 }, 30000);
 
 test('test solved', async () => {
     let solved, ngm;
-    await gamePage.setBoard([1, 2, null, 3]);
-    ngm = await gamePage.getNewGameModal();
-    await ngm.setSize(2);
+    await startGame([1, 2, null, 3]);
 
     // check that modal is closed
     await gamePage.waitForSolvedToClose();
@@ -88,12 +113,12 @@ test('test solved', async () => {
     await expect(gamePage.getButtonsStatus()).resolves.toEqual([false, false]);
     await gamePage.clickTile('3');
     await expect(gamePage.getTiles()).resolves.toEqual([['1', '2'], ['3']]);
+
+    await validateLogs();
 }, 30000);
 
 test('test reset and undo', async () => {
-    await gamePage.setBoard([1, 2, 3, 4, 5, 6, 7, null, 8]);
-    const ngm = await gamePage.getNewGameModal();
-    await ngm.setSize(3);
+    await startGame([1, 2, 3, 4, 5, 6, 7, null, 8]);
 
     // disabled on start
     await expect(gamePage.getButtonsStatus()).resolves.toEqual([false, false]);
@@ -121,13 +146,12 @@ test('test reset and undo', async () => {
     await expect(gamePage.getTiles()).resolves.toEqual([['1', '2', '3'], ['4', '5', '6'], ['7', undefined, '8']]);
     await expect(gamePage.getButtonsStatus()).resolves.toEqual([false, false]);
 
+    await validateLogs();
 }, 30000);
 
 test('full 3x3 game play', async () => {
     let ngm, solved;
-    await gamePage.setBoard([1, 2, null, 5, 6, 3, 4, 7, 8]);
-    ngm = await gamePage.getNewGameModal();
-    await ngm.setSize(3);
+    await startGame([1, 2, null, 5, 6, 3, 4, 7, 8]);
 
     // check moving tile back and forth
     await expect(gamePage.getTiles()).resolves.toEqual([['1', '2'], ['5', '6', '3'], ['4', '7', '8']]);
@@ -168,11 +192,12 @@ test('full 3x3 game play', async () => {
     ngm = await gamePage.clickNewGame();
     await ngm.clickStart();
     await expect(gamePage.getTiles()).resolves.toEqual([['1', '2', '3'], ['4', '5', '6'], ['7', undefined, '8']]);
+
+    await validateLogs();
 }, 30000);
 
 
 // remove sleep from wait for
-// add startNewGame test
 // check log errors
 // snapshot on error
 // re-use browser

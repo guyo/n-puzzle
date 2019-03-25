45c843b51a6dcecb9999e7cac02c48067bdb0cca
guyo <offer.guy@gmail.com>
2019-03-25 14:32:23 +0200

    additional fixes to NewGameModal.js

--- src/app/components/NewGameModal.js
+++ src/app/components/NewGameModal.js
@@ -1,38 +1,25 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import {
-    Modal, Button, FormGroup, FormControl,
-    ControlLabel, HelpBlock
-} from 'react-bootstrap';
+import { Modal, Button, FormGroup, FormControl, ControlLabel, HelpBlock } from 'react-bootstrap';
 
 const NewGameModal = (props) => {
-
-    const [state, setState] = useState({ size: props.defaultSize });
-
-    function handleChange(e) {
-        setState({ size: e.target.value });
-    }
-
-    function handleClose() {
-        props.onClose();
-    }
-
-    const canClose = props.canClose;
-    const sizeNum = +state.size;
+    const [inputVal, setInputVal] = useState(props.defaultSize);
+    const sizeNum = +inputVal; //change input value into a number
     const valid = sizeNum >= props.minSize && sizeNum <= props.maxSize;
     const onSubmit = () => valid && props.onSubmit(sizeNum);
 
     return (
-        <Modal show={props.show} onHide={handleClose}
-            backdrop={canClose ? true : 'static'} id='newGameModal'>
-            <Modal.Header closeButton={canClose}>
+        <Modal show={props.show} onHide={props.onClose}
+            backdrop={props.canClose ? true : 'static'} id='newGameModal'>
+            <Modal.Header closeButton={props.canClose}>
                 <Modal.Title>{props.title}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <form onSubmit={(e) => { e.preventDefault(), onSubmit(); }} >
                     <FormGroup validationState={valid ? null : 'error'}>
                         <ControlLabel>Choose Puzzle Size:</ControlLabel>
-                        <FormControl type='text' value={state.size} onChange={handleChange} autoFocus={true} />
+                        <FormControl type='text' autoFocus={true}
+                            value={inputVal} onChange={e => setInputVal(e.target.value)}/>
                         <FormControl.Feedback />
                         {!valid && <HelpBlock>Enter a number between 3 and 12</HelpBlock>}
                     </FormGroup>
@@ -42,7 +29,7 @@ const NewGameModal = (props) => {
                 <Button bsStyle='success' onClick={onSubmit}
                     disabled={!valid} id='newgamestart'>Start Game !</Button>
 
-                {canClose && <Button onClick={handleClose} id='newgamecancel'>{'Cancel'}</Button>}
+                {props.canClose && <Button onClick={props.onClose} id='newgamecancel'>{'Cancel'}</Button>}
             </Modal.Footer>
         </Modal>
     );

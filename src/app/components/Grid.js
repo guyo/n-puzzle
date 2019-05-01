8250be1f1573331784b6292b7637b81469fd44ed
guyo <offer.guy@gmail.com>
2019-05-01 16:16:52 +0300

    fix memoization

--- src/app/components/Grid.js
+++ src/app/components/Grid.js
@@ -1,40 +1,30 @@
-import React, { useState } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Tile from './Tile';
 
-// custom hook for memoizing the callbacks passed to tiles solving 2 issues:
-// memozie callback per index and update the master callback without changing the callbacks 
-function useCallbackFactory(masterCallback) {
-    const [callbackFactory, _] = useState({ masterCallback, callbacks: [] });
-    if (callbackFactory.masterCallback !== masterCallback)
-        callbackFactory.masterCallback = masterCallback;
-    const callbacks = callbackFactory.callbacks;
-    return (index) => callbacks[index] || (callbacks[index] = () => callbackFactory.masterCallback(index));
-}
 
-const Grid = (props) => {
-    const columns = props.columns;
-    const rows = Math.ceil(props.tiles.length / columns); // calculate # of rows based on columns
+const Grid = ({columns, height, width, tiles, onTileClicked}) => {
+    const rows = Math.ceil(tiles.length / columns); // calculate # of rows based on columns
 
-    const tileWidth = Math.floor(props.width / columns);
-    const tileHeight = Math.floor(props.height / rows);
+    const tileWidth = Math.floor(width / columns);
+    const tileHeight = Math.floor(height / rows);
 
     // round the original width & height  to adjust to the # of tiles
     // then increase by one to make sure a small edge is left on the right & bottom side.
-    const width = tileWidth * columns + 1;
-    const height = tileHeight * rows + 1;
     const style = {
-        backgroundColor: 'darkgrey',
-        width: `${width}px`,
-        height: `${height}px`,
-        position: 'relative'
+        width: `${tileWidth * columns + 1}px`,
+        height: `${tileHeight * rows + 1}px`,
+        position: 'relative',
+        backgroundColor: 'darkgrey'
     };
 
-    const getCallback = useCallbackFactory(props.onTileClicked);
+    // create memoized list of callbacks for the tiles to prevent them from re-rendering
+    const callbacks=useMemo(() => [...Array(tiles.length).keys()].map(i => () => onTileClicked(i)),
+        [tiles.length, onTileClicked]);
 
     return (
         <div style={style} id='grid'>
-            {props.tiles.map((value, index) => {
+            {tiles.map((value, index) => {
                 if (value !== null) {
                     const row = Math.floor(index / columns);
                     const column = index % columns;
@@ -42,7 +32,7 @@ const Grid = (props) => {
                     return (<Tile key={value} value={value}
                         left={column * tileWidth + 2} top={row * tileHeight + 2}
                         height={tileHeight} width={tileWidth} borderWidth={2}
-                        onClick={getCallback(index)}
+                        onClick={callbacks[index]}
                     />);
                 } else
                     return null;
--- src/app/containers/GridContainer.js
+++ src/app/containers/GridContainer.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { moveTile } from '../actions/gameActions';
-import React from 'react';
+import React, { useRef, useEffect ,useCallback } from 'react';
 import Grid from '../components/Grid.js';
 import { getCheckMove } from '../selectors';
 
@@ -12,16 +12,26 @@ const mapStateToProps = (state, { width, height }) => {
 // count on conncet to auto bind the action creator to dispatch
 const mapDispatchToProps = { moveTile };
 
+// custom hook for creating a static callback , so grid is not rerendered on every render
+function useEventCallback(callback) {
+    const callbackRef=useRef();
+    useEffect(() => {
+        callbackRef.current=callback;
+    }, [callback]);
+
+    return useCallback((...args) => (0,callbackRef.current)(args), [callbackRef]);
+}
+
 // create an onTileClicked action which check for move validity
 // and fires a moveTile action with the right property when it is
 const GridContainer = ({ board, size, height, width, checkMove, moveTile }) => (
     <Grid tiles={board} columns={size}
         height={height} width={width}
-        onTileClicked={(tilePos) => {
+        onTileClicked={useEventCallback((tilePos) => {
             const to = checkMove(tilePos);
             if (to >= 0) // only execute valid moves
                 moveTile(tilePos, to);
-        }}
+        })}
     />
 );
 

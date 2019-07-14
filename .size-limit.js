e828180b1d9e7b94af7e7819a52761f8202f044d
guyo <offer.guy@gmail.com>
2019-07-14 15:10:58 +0300

    update size limit to inspect css

--- .size-limit.js
+++ .size-limit.js
@@ -1,10 +1,18 @@
 module.exports = [
   {
-    name: "Main",
+    name: "javascript",
     gzip: false,
     webpack: false,
     running: false,
     path: "dist/*.js.gz",
     limit: "100 KB"
+  },
+  {
+    name: "css",
+    gzip: false,
+    webpack: false,
+    running: false,
+    path: "dist/*.css.gz",
+    limit: "22 KB"
   }
 ]
